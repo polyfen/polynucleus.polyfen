5ce6d8e535ebb0ec829a727768d77d1b3fbffe16
xxiicode <leandrobilokapic@gmail.com>
2023-09-27 15:48:00 +0200

    Update call-to-action.php

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,9 +1,7 @@
 <section>
   <div id="cta-content">
     <h2>Give your customers<br> the experience they deserve</h2>
-
-      <a href="contact.php#message">end a message</a>
-      <a href="contact.php#call">schedule a call</a>
-      </a>
+    <a href="contact.php#message">end a message</a>
+    <a href="contact.php#call">schedule a call</a>
   </div>
 </section>
