2cdedf510d4d5e51630cc82b9f8e7316a4f2f92e
xxiicode <leandrobilokapic@gmail.com>
2023-09-29 13:44:48 +0200

    new changes

--- index.php
+++ index.php
@@ -1,178 +1,197 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <title>Polyfen | Creative & Development Agency</title>
+<head>
+  <title>Polyfen | Creative & Development Agency</title>
 
 
-    <!-- INCLUDE HEAD.PHP start -->
-      <?php include 'includes/head.php';?>
-    <!-- INCLUDE HEAD.PHP end -->
+  <!-- INCLUDE HEAD.PHP start -->
+  <?php include 'includes/head.php';?>
+  <!-- INCLUDE HEAD.PHP end -->
 
-    <!-- PAGE-SPECIFIC META start -->
-    <meta property="og:title" content="Polyfen | Creative &amp; Development Agency">
-    <meta property="og:url" content="https://polyfen.com">
-    <meta name="keywords" content="Brand, Brand Identity Design, Branding, Agency, Graphic Design, Logo, Creative, Strategy, Consultancy, Business Growth" >
-    <meta name="description" property="og:description" content="We offer design, marketing, and software development services for emerging businesses from the US." >
-    <meta property="og:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png">
-    <meta property="og:image:width" content="1200" >
-    <meta property="og:image:height" content="630" >
-    <meta property="fb:app_id" content="1799272553516195" >
-    <meta name="twitter:card" content="summary_large_image" >
-    <meta name="twitter:site" content="Polyfen" >
-    <meta name="twitter:creator" content="@PolyfenAgency">
-    <meta name="twitter:title" content="Polyfen | Creative &amp; Development Agency" >
-    <meta name="twitter:description" content="We offer design, marketing, and software development services for emerging businesses from the US." >
-    <meta name="twitter:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png" >
-    <meta name="robots" content="index, follow">
-    <!-- PAGE-SPECIFIC META end -->
+  <!-- PAGE-SPECIFIC META start -->
+  <meta property="og:title" content="Polyfen | Creative &amp; Development Agency">
+  <meta property="og:url" content="https://polyfen.com">
+  <meta name="keywords"
+    content="Brand, Brand Identity Design, Branding, Agency, Graphic Design, Logo, Creative, Strategy, Consultancy, Business Growth">
+  <meta name="description" property="og:description"
+    content="We offer design, marketing, and software development services for emerging businesses from the US.">
+  <meta property="og:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+  <meta property="fb:app_id" content="1799272553516195">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="Polyfen">
+  <meta name="twitter:creator" content="@PolyfenAgency">
+  <meta name="twitter:title" content="Polyfen | Creative &amp; Development Agency">
+  <meta name="twitter:description"
+    content="We offer design, marketing, and software development services for emerging businesses from the US.">
+  <meta name="twitter:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png">
+  <meta name="robots" content="index, follow">
+  <!-- PAGE-SPECIFIC META end -->
 
-    <!-- PAGE-SPECIFIC CSS start -->
-    <!-- PAGE-SPECIFIC CSS end -->
+  <!-- PAGE-SPECIFIC CSS start -->
+  <link rel="stylesheet" href="app/css/reset.css">
+  <link rel="stylesheet" href="app/css/styles.css">
+  <link rel="stylesheet" href="app/css/typography.css">
+  <!-- PAGE-SPECIFIC CSS end -->
 
-    <!-- PAGE-SPECIFIC SCRIPTS start -->
-    <!-- PAGE-SPECIFIC SCRIPTS end -->
+  <!-- PAGE-SPECIFIC SCRIPTS start -->
+  <!-- PAGE-SPECIFIC SCRIPTS end -->
+</head>
 
-  </head>
+<body>
 
-  <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+      style="display:none;visibility:hidden">
+    </iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+  <!-- FORCE MAX-WIDTH 100% start -->
+    <!-- INCLUDE HELLO-BAR.PHP start -->
+    <?php include 'includes/hello-bar.php';?>
+    <!-- INCLUDE HELLO-BAR.PHP end -->
 
-    <!-- FORCE MAX-WIDTH 100% start -->
-    <div class="main-wrapper">
+    <!-- INCLUDE HEADER.PHP start -->
+    <?php include 'includes/header.php';?>
+    <!-- INCLUDE HEADER.PHP end -->
 
-      <!-- INCLUDE HELLO-BAR.PHP start -->
-        <?php include 'includes/hello-bar.php';?>
-      <!-- INCLUDE HELLO-BAR.PHP end -->
-
-      <!-- INCLUDE HEADER.PHP start -->
-        <?php include 'includes/header.php';?>
-      <!-- INCLUDE HEADER.PHP end -->
-
-      <!-- COVER start -->
-      <section>
-        <div>
-          <h1>Unleash your business<br>full potential</h1>
-
-          <div>
-            <p>We are a creative agency offering design, marketing, and software development services for emerging businesses.</p>
-            <small>Don't be shy</small>
-            <a href="/contact.php">Let's Talk!</a>
-          </div>
-          <!-- <div class="background">
-            <div id="pearl-11" class="pearl rotate-slower">
-            </div>
-            <div id="pearl-12" class="pearl rotate">
-            </div>
-            <div id="pearl-13" class="pearl rotate">
-            </div>
-          </div> -->
-        </div>
-      </section>
-      <!-- COVER end -->
-
+    <!-- COVER start -->
+  <main class="container">
+    <section class="cover align-center">
       <div>
+        <h1 class="title-1">Unleash your business<br>full potential</h1>
+        <p>We are a fully-remote creative &amp; development agency helping emerging 
+          businesses revamp their brand, web, and apps since 2018.</p>
+        <small>Don't be shy</small>
+        <a href="/contact.php">Let's Talk!</a>
+      </div>
+      <!-- 
+      <div class="background">
+        <div id="pearl-11" class="pearl rotate-slower"></div>
+        <div id="pearl-12" class="pearl rotate"></div>
+        <div id="pearl-13" class="pearl rotate"></div>
+      </div>
+      -->
+    </section>
+    <!-- COVER end -->
 
-        <!-- FEATURED PROJECTS start -->
-        <section>
-          <h2>Work</h2>
-          <h3>Featured projects</h3>
+    <div><!--div fantasma -------------------------------------------------- -->
 
-          <!-- BIKE IN MEX start -->
-          <div class="hidden featured-project">
-            <div id="bike-in-mex-cover" class="project-cover" onmouseover="document.getElementById('bike-in-mex-button').className='button-hover';document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-button').className='';document.getElementById('bike-in-mex-cover').className='project-cover';">
+      <!-- FEATURED PROJECTS start -->
+      <section>
+        <h2>Work</h2>
+        <h3>Featured projects</h3>
+
+        <!-- BIKE IN MEX start -->
+        <div class="hidden featured-project">
+          <div id="bike-in-mex-cover" class="project-cover"
+            onmouseover="document.getElementById('bike-in-mex-button').className='button-hover';document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';"
+            onmouseout="document.getElementById('bike-in-mex-button').className='';document.getElementById('bike-in-mex-cover').className='project-cover';">
+            <a href="/bike-in-mex.php">
+              <figure>
+                src="imgs/bike-in-mex-thumbnail.webp" loading="lazy" alt="Bike in Mex flag">
+              </figure>
+            </a>
+            <div class="project-preview">
               <a href="/bike-in-mex.php">
                 <figure>
-                   src="imgs/bike-in-mex-thumbnail.webp" loading="lazy" alt="Bike in Mex flag">
+                  <img src="imgs/bike-in-mex-preview.gif" loading="lazy" alt="">
                 </figure>
               </a>
-              <div class="project-preview">
-                <a href="/bike-in-mex.php">
-                  <figure>
-                    <img src="imgs/bike-in-mex-preview.gif" loading="lazy" alt="">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4>Bike in Mex</h4>
-              <p>We designed an identity focused on geometric illustrations inspired by ancient Aztec and Mayan symbols set on the vibrant colors of Mexico's flag for a bicycle rental shop based in Tulum, Mexico.</p>
-              <a href="/bike-in-mex.php" onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
-                View project</a>
             </div>
           </div>
-          <!-- BIKE IN MEX end -->
+          <div class="project-description">
+            <h4>Bike in Mex</h4>
+            <p>We designed an identity focused on geometric illustrations inspired by ancient Aztec and Mayan symbols
+              set on the vibrant colors of Mexico's flag for a bicycle rental shop based in Tulum, Mexico.</p>
+            <a href="/bike-in-mex.php"
+              onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';"
+              onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
+              View project</a>
+          </div>
+        </div>
+        <!-- BIKE IN MEX end -->
 
-          <!-- TRADEANA start -->
-          <div class="hidden featured-project">
-            <div id="tradeana-cover" class="project-cover" onmouseover="document.getElementById('tradeana-button').className='button-hover';document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-button').className='';document.getElementById('tradeana-cover').className='project-cover';">
+        <!-- TRADEANA start -->
+        <div class="hidden featured-project">
+          <div id="tradeana-cover" class="project-cover"
+            onmouseover="document.getElementById('tradeana-button').className='button-hover';document.getElementById('tradeana-cover').className='project-cover project-cover-hover';"
+            onmouseout="document.getElementById('tradeana-button').className='';document.getElementById('tradeana-cover').className='project-cover';">
+            <a href="/tradeana.php">
+              <figure>
+                <img src="imgs/tradeana-thumbnail.webp" loading="lazy" alt="">
+              </figure>
+            </a>
+            <div class="project-preview">
               <a href="/tradeana.php">
                 <figure>
-                  <img src="imgs/tradeana-thumbnail.webp" loading="lazy" alt="">
+                  <img src="imgs/tradeana-preview.gif" loading="lazy" alt="">
                 </figure>
               </a>
-              <div class="project-preview">
-                <a href="/tradeana.php">
-                  <figure>
-                    <img src="imgs/tradeana-preview.gif" loading="lazy" alt="">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4>Tradeana</h4>
-              <p>Tradeana's patented software, Forex Risk Engine™, is designed by traders for traders to help them efficiently audit and manage the risk of their ForEx operations.</p>
-              <a href="/tradeana.php" onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
-                View project</a>
             </div>
           </div>
-          <!-- TRADEANA end -->
+          <div class="project-description">
+            <h4>Tradeana</h4>
+            <p>Tradeana's patented software, Forex Risk Engine™, is designed by traders for traders to help them
+              efficiently audit and manage the risk of their ForEx operations.</p>
+            <a href="/tradeana.php"
+              onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';"
+              onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
+              View project</a>
+          </div>
+        </div>
+        <!-- TRADEANA end -->
 
-          <!-- VILLA TEREZIJA start -->
-          <div class="hidden featured-project">
-            <div id="villa-terezija-cover" class="project-cover" onmouseover="document.getElementById('villa-terezija-button').className='button-hover';document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-button').className='';document.getElementById('villa-terezija-cover').className='project-cover';">
+        <!-- VILLA TEREZIJA start -->
+        <div class="hidden featured-project">
+          <div id="villa-terezija-cover" class="project-cover"
+            onmouseover="document.getElementById('villa-terezija-button').className='button-hover';document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';"
+            onmouseout="document.getElementById('villa-terezija-button').className='';document.getElementById('villa-terezija-cover').className='project-cover';">
+            <a href="/villa-terezija.php">
+              <figure>
+                <img src="imgs/villa-terezija-thumbnail.webp" loading="lazy" alt="">
+              </figure>
+            </a>
+            <div class="project-preview">
               <a href="/villa-terezija.php">
                 <figure>
-                  <img src="imgs/villa-terezija-thumbnail.webp" loading="lazy" alt="">
+                  <img src="imgs/villa-terezija-preview.gif" loading="lazy" alt="">
                 </figure>
               </a>
-              <div class="project-preview">
-                <a href="/villa-terezija.php">
-                  <figure>
-                   <img src="imgs/villa-terezija-preview.gif" loading="lazy" alt="">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4>Villa Terezija</h4>
-              <p>A touristic accommodation surrounded by a stunning rose garden in the quiet town of Baška on Krk Island, Croatia. Minutes walk from a sandy beach on the clear blue Adriatic Sea.</p>
-              <a href="/villa-terezija.php" onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';"
-                id="villa-terezija-button">View project</a>
             </div>
           </div>
-          <!-- VILLA TEREZIJA end -->
+          <div class="project-description">
+            <h4>Villa Terezija</h4>
+            <p>A touristic accommodation surrounded by a stunning rose garden in the quiet town of Baška on Krk Island,
+              Croatia. Minutes walk from a sandy beach on the clear blue Adriatic Sea.</p>
+            <a href="/villa-terezija.php"
+              onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';"
+              onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';"
+              id="villa-terezija-button">View project</a>
+          </div>
+        </div>
+        <!-- VILLA TEREZIJA end -->
 
-        </section>
-        <!-- FEATURED PROJECTS end -->
-      </div> <!-- div fantasma -->
+      </section>
+      <!-- FEATURED PROJECTS end -->
+    </div> <!-- div fantasma -->
 
-    </div> <!-- main wrapper end -->
+  </main> <!-- main wrapper end -->
 
-    <!-- FORCE MAX-WIDTH 100% end -->
+  <!-- FORCE MAX-WIDTH 100% end -->
 
-    <!-- INCLUDE CALL-TO-ACTION.PHP start -->
-      <?php include 'includes/call-to-action.php';?>
-    <!-- INCLUDE CALL-TO-ACTION.PHP end -->
+  <!-- INCLUDE CALL-TO-ACTION.PHP start -->
+  <?php include 'includes/call-to-action.php';?>
+  <!-- INCLUDE CALL-TO-ACTION.PHP end -->
 
-    <!-- INCLUDE FOOTER.PHP start -->
-      <?php include 'includes/footer.php';?>
-    <!-- INCLUDE FOOTER.PHP end -->
+  <!-- INCLUDE FOOTER.PHP start -->
+  <?php include 'includes/footer.php';?>
+  <!-- INCLUDE FOOTER.PHP end -->
 
-  </body>
+</body>
 
 </html>
